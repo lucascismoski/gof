848cdd964fa864016d2f50493ae602b1df7e5b79
lucascismoski <lucascismoski@gmail.com>
2024-06-16 00:23:34 -0300

    Revert "commit inicial"
    This reverts commit fefa5d71f6e3ef9d64095508e8f03ff6d55e0904.

--- src/main/java/com/lcfl/gof/model/EnderecoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lcfl.gof.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, String> {
-}
